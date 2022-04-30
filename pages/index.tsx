0c34fd05a911e38f2370331bcb1729ea236436a1
YJQ <vanessa@YJQs-MacBook-Pro.local>
2022-04-29 20:45:55 -0400

    delete unnecessary icons and files

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,16 +39,10 @@ const Home: NextPage = () => {
       <div className={styles.content}></div>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Developed by{' '}
           <span className={styles.logo}>
             Group xx
           </span>
-        </a>
       </footer>
     </div >
   )
